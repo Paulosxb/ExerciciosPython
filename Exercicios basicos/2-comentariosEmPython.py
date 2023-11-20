918c58330bbe6315b3166b46fc44998673e66975
Paulo Sergio <paulosxb@gmail.com>
2023-11-20 16:00:20 -0300

    atualizando
    aprendendo como colocar comentarios

--- Exercicios basicos/2-comentariosEmPython.py
+++ Exercicios basicos/2-comentariosEmPython.py
@@ -1,1 +1,5 @@
 #Exemplo de comentarios simples de até uma linha
+
+'''Todo comentario maior que uma linha em Python deve ter seu conteudo estre aspas triplas simpls ou aspas triplas duplas'''
+
+#Comentários de múltiplas linhas, usando o caractere '#' para indicar a quebra de linha
