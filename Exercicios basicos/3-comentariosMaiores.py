6f18c9b4e1f8730939ea8ade21124f651874ce04
Paulo Sergio <paulosxb@gmail.com>
2024-01-11 12:18:24 -0300

    Crie um comentario de mais de uma linha
    Comentario maior que uma linha

---
+++ Exercicios basicos/3-comentariosMaiores.py
@@ -0,0 +1,2 @@
+'''Todo comentario maior que uma linha em Pythhon deve ter seu conteudo entre aspas triplas simples ''' ''' ou 
+aspas triplas duplas """ """ de acordo com a preferencia do desenvolvedor. '''
